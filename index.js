773b09420b6d850b35ad9f5da9b569322e1ef393
DVDVDIP <fbgmlwo1@naver.com>
2023-04-03 17:00:19 +0900

    시간정보업뎃

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { format } = require('date-fns');
 var d = new Date();
 console.log(d); // 2023-04-01T13:29:45.171Z
-
+//포멧
 var formatedD = format(d, 'yyyy-MM-dd');
 console.log(formatedD); // 2023-04-01
